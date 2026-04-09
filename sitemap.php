45bc6bfbf69956d136f05f51b6478ec2b003a866
BaldEagle1970 <11087691@uvu.edu>
2026-04-09 17:10:43 -0600

    update robot and site map

---
+++ sitemap.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/includes/functions.php';
+
+header('Content-Type: application/xml; charset=UTF-8');
+
+$exclude = ['privacy-policy', 'terms'];
+$uniquePages = [];
+foreach (page_catalog() as $slug => $item) {
+    if (in_array($slug, $exclude, true)) {
+        continue;
+    }
+
+    $path = (string) ($item['path'] ?? '');
+    if ($path === '' || isset($uniquePages[$path])) {
+        continue;
+    }
+
+    $uniquePages[$path] = [
+        'slug' => $slug,
+        'path' => $path,
+    ];
+}
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<?php foreach ($uniquePages as $item): ?>
+  <url>
+    <loc><?= e(absolute_url($item['path'])) ?></loc>
+    <lastmod><?= e(page_lastmod_iso8601($item['slug'], $item['path'])) ?></lastmod>
+  </url>
+<?php endforeach; ?>
+</urlset>
